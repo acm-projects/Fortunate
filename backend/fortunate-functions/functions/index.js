3659a65b9c319f1a33d1c1e7103254262cb7ac11
caleb-chou <haloisawesome412@gmail.com>
2021-02-28 23:28:31 -0600

    Added route for trades (WIP)

--- backend/fortunate-functions/functions/index.js
+++ backend/fortunate-functions/functions/index.js
@@ -1,10 +1,13 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { login, signup } = require("./handlers/users");
+const { login, signup, trade } = require("./handlers/users");
+
+const {FBAuth} = require('./util/fbauth');
 
 // Users Routes
 app.post("/login", login);
 app.post("/signup", signup);
+app.post("/trade",FBAuth, trade);
 
 exports.api = functions.https.onRequest(app);
