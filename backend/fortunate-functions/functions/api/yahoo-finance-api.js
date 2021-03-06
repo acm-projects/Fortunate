e2d8380fc8d6a3356a911ac67ba997069a6404db
Nikita Istomin <nyi180000@utdallas.edu>
2021-03-06 16:12:40 -0600

    Changing to new api in progress

--- backend/fortunate-functions/functions/api/yahoo-finance-api.js
+++ backend/fortunate-functions/functions/api/yahoo-finance-api.js
@@ -3,63 +3,66 @@ const app = require('express')();
 const db = require('../util/admin').admin.firestore();
 
 const API_KEY = require('../config.json').API_KEY;
-
+const lang = "en";
+const region = "US";
 
 
 /**
- *  Saves the live summary information of market by region to the database
+ *  Get live market summary information at the request time
  */ 
-exports.marketSummary = (req, res) => {
+exports.getMarketSummary = (req, res) => {
     unirest
-        .get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-summary")
+        .get("https://yahoo-finance-low-latency.p.rapidapi.com/v6/finance/quote/marketSummary")
         .query({
-            "region": "US"
+            "region": region,
+            "lang": lang
         }).headers({
             "x-rapidapi-key": API_KEY,
-            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+            "x-rapidapi-host": "yahoo-finance-low-latency.p.rapidapi.com",
             "useQueryString": true
         }).end((result) => {
+            console.log(result.error);
             if (result.error) throw new Error(result.error);
-            // console.log(result.body.marketSummaryAndSparkResponse);
-            db.collection('api').doc('market').set(result.body.marketSummaryAndSparkResponse)
-            .then(() => {
-                return res.status(200).json({success: "Success"});
-            }).catch(error => {
-                console.error(error);
-                return res.status(500).json({error: error.code}); 
-            })
+            console.log(result.body.marketSummaryResponse);
+            // db.collection('api').doc('market').set(result.body.marketSummaryResponse.result)
+            // .then(() => {
+            //     return res.status(200).json({success: "Success"});
+            // }).catch(error => {
+            //     console.error(error);
+            //     return res.status(500).json({error: error.code}); 
+            // })
         });
 }
 
 
-/**
- *  Saves the the quotes by symbols to the database
- */ 
-exports.getQuotes = (req, res) => {
-    let symbols;
-    for(key in req.body) {
-        symbols += req.body[key] + ",";
-    }
-    symbols = symbols.slice(0, symbols.length - 1); //delete the last comma 
+// /**
+//  *  Saves the the quotes by symbols to the database
+//  */ 
+// exports.getQuotes = (req, res) => {
+//     let symbols;
+//     for(key in req.body) {
+//         symbols += req.body[key] + ",";
+//     }
+//     symbols = symbols.slice(0, symbols.length - 1); //delete the last comma 
 
-    unirest
-        .get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes")
-        .query({
-            "region": "US",
-            "symbols": symbols
-        }).headers({
-            "x-rapidapi-key": API_KEY,
-            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-            "useQueryString": true
-        }).end((result) => {
-            if (result.error) throw new Error(result.error);
-            // console.log(result.body.quoteResponse);
-            db.collection('api').doc('quotes').set(result.body.quoteResponse, {merge: true})
-            .then(() => {
-                return res.status(200).json({success: "Success"});
-            }).catch(error => {
-                console.error(error);
-                return res.status(500).json({error: error.code}); 
-            })
-        });
-}
+//     unirest
+//         .get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes")
+//         .query({
+//             "region": "US",
+//             "symbols": symbols
+//         }).headers({
+//             "x-rapidapi-key": API_KEY,
+//             "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+//             "useQueryString": true
+//         }).end((result) => {
+//             if (result.error) throw new Error(result.error);
+//             // console.log(result.body.quoteResponse);
+//             db.collection('api').doc('quotes').set(result.body.quoteResponse, {merge: true})
+//             .then(() => {
+//                 return res.status(200).json({success: "Success"});
+//             }).catch(error => {
+//                 console.error(error);
+//                 return res.status(500).json({error: error.code}); 
+//             })
+//         });
+// }
--- backend/fortunate-functions/functions/index.js
+++ backend/fortunate-functions/functions/index.js
@@ -2,13 +2,13 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const { login, signup } = require("./handlers/users");
-const { marketSummary, getQuotes } = require("./api/yahoo-finance-api");
+const { getMarketSummary } = require("./api/yahoo-finance-api");
 
 // Users Routes
 app.post("/login", login);
 app.post("/signup", signup);
 
-app.post("/market-summary", marketSummary);
-app.post("/get-quotes", getQuotes);
+app.post("/market-summary", getMarketSummary);
+// app.post("/get-quotes", getQuotes); 
 
 exports.api = functions.https.onRequest(app);
