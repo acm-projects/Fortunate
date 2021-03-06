42abb3ce18ce708576a31b42c5a5d72d8b3d5ae8
Nikita Istomin <nyi180000@utdallas.edu>
2021-03-05 23:17:54 -0600

    Market summary function V 1.0

--- backend/fortunate-functions/functions/api/yahoo-finance-api.js
+++ backend/fortunate-functions/functions/api/yahoo-finance-api.js
@@ -4,18 +4,39 @@ const app = require('express')();
 const API_KEY = require('../config.json').API_KEY;
 const region = "US";
 
-app.get('/market-summary', (req, res) => {
 
-});
+
+
+exports.marketSummary = (req, res) => {
+    const db = require('../util/admin').admin.firestore();
+    const result = getMarketSummary;
+
+    db.doc('/api/market').set(result)
+    .then(doc => {
+        return res.status(200).json({token});
+    }).catch(err => {
+        console.error(error);
+        return res.status(500).json({error: error.code}); 
+    })
+}
+
+
+
+
+
+
+
+
 
 
 /**
- * Get live summary information of market by region and log it into console.
+ * Returns the live summary information of market by region
  */
-exports.getMarketSummary = unirest.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-summary?
+getMarketSummary = unirest.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-summary?
     region=${region}`)
         .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
         .header("X-RapidAPI-Key", API_KEY)
         .end(result => {
-            console.log(result.status, result.headers, result.body);
+            return {status: result.status, headers: result.headers, body: result.body};
     });
+
--- backend/fortunate-functions/functions/index.js
+++ backend/fortunate-functions/functions/index.js
@@ -2,9 +2,12 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const { login, signup } = require("./handlers/users");
+const marketSummary = require("./api/yahoo-finance-api")
 
 // Users Routes
 app.post("/login", login);
 app.post("/signup", signup);
+
+app.post("/market-summary", marketSummary);
 
 exports.api = functions.https.onRequest(app);
