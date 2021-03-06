2f37a6b133da0a7ae814c674c333844b535aa29a
Nikita Istomin <nyi180000@utdallas.edu>
2021-03-06 04:09:15 -0600

    Wrote marketSummary and getQuotes functions

--- backend/fortunate-functions/functions/api/yahoo-finance-api.js
+++ backend/fortunate-functions/functions/api/yahoo-finance-api.js
@@ -1,42 +1,65 @@
 const unirest = require('unirest');
 const app = require('express')();
+const db = require('../util/admin').admin.firestore();
 
 const API_KEY = require('../config.json').API_KEY;
-const region = "US";
-
-
-
-
-exports.marketSummary = (req, res) => {
-    const db = require('../util/admin').admin.firestore();
-    const result = getMarketSummary;
-
-    db.doc('/api/market').set(result)
-    .then(doc => {
-        return res.status(200).json({token});
-    }).catch(err => {
-        console.error(error);
-        return res.status(500).json({error: error.code}); 
-    })
-}
-
-
-
-
-
-
-
 
 
 
 /**
- * Returns the live summary information of market by region
- */
-getMarketSummary = unirest.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-summary?
-    region=${region}`)
-        .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
-        .header("X-RapidAPI-Key", API_KEY)
-        .end(result => {
-            return {status: result.status, headers: result.headers, body: result.body};
-    });
+ *  Saves the live summary information of market by region to the database
+ */ 
+exports.marketSummary = (req, res) => {
+    unirest
+        .get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-summary")
+        .query({
+            "region": "US"
+        }).headers({
+            "x-rapidapi-key": API_KEY,
+            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+            "useQueryString": true
+        }).end((result) => {
+            if (result.error) throw new Error(result.error);
+            // console.log(result.body.marketSummaryAndSparkResponse);
+            db.collection('api').doc('market').set(result.body.marketSummaryAndSparkResponse)
+            .then(() => {
+                return res.status(200).json({success: "Success"});
+            }).catch(error => {
+                console.error(error);
+                return res.status(500).json({error: error.code}); 
+            })
+        });
+}
 
+
+/**
+ *  Saves the the quotes by symbols to the database
+ */ 
+exports.getQuotes = (req, res) => {
+    let symbols;
+    for(key in req.body) {
+        symbols += req.body[key] + ",";
+    }
+    symbols = symbols.slice(0, symbols.length - 1); //delete the last comma 
+
+    unirest
+        .get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes")
+        .query({
+            "region": "US",
+            "symbols": symbols
+        }).headers({
+            "x-rapidapi-key": API_KEY,
+            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+            "useQueryString": true
+        }).end((result) => {
+            if (result.error) throw new Error(result.error);
+            // console.log(result.body.quoteResponse);
+            db.collection('api').doc('quotes').set(result.body.quoteResponse, {merge: true})
+            .then(() => {
+                return res.status(200).json({success: "Success"});
+            }).catch(error => {
+                console.error(error);
+                return res.status(500).json({error: error.code}); 
+            })
+        });
+}
--- backend/fortunate-functions/functions/index.js
+++ backend/fortunate-functions/functions/index.js
@@ -2,12 +2,13 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const { login, signup } = require("./handlers/users");
-const marketSummary = require("./api/yahoo-finance-api")
+const { marketSummary, getQuotes } = require("./api/yahoo-finance-api");
 
 // Users Routes
 app.post("/login", login);
 app.post("/signup", signup);
 
 app.post("/market-summary", marketSummary);
+app.post("/get-quotes", getQuotes);
 
 exports.api = functions.https.onRequest(app);
