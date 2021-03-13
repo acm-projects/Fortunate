66876bfb1fc9a413dc784183d8a2246e7944756e
Nikita Istomin <nyi180000@utdallas.edu>
2021-03-13 13:32:50 -0600

    get-ticker route

--- backend/fortunate-functions/functions/index.js
+++ backend/fortunate-functions/functions/index.js
@@ -2,13 +2,15 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const { login, signup } = require("./handlers/users");
-const { getMarketSummary, getQuotes } = require("./api/yahoo-finance-api");
+const { getMarketSummary, getQuotes } = require("./util/yahooapi");
 
 // Users Routes
 app.post("/login", login);
 app.post("/signup", signup);
 
+// API routs 
 app.post("/market-summary", getMarketSummary);
 app.post("/get-quotes", getQuotes); 
+app.post("/get-ticker", getTicker)
 
 exports.api = functions.https.onRequest(app);
--- backend/fortunate-functions/functions/util/yahooapi.js
+++ backend/fortunate-functions/functions/util/yahooapi.js
@@ -1,6 +1,6 @@
 const unirest = require('unirest');
 const app = require('express')();
-const db = require('../util/admin').admin.firestore();
+const db = require('./admin').admin.firestore();
 
 const API_KEY = require('../config.json').API_KEY;
 const lang = "en";
@@ -67,6 +67,63 @@ exports.getQuotes = (req, res) => {
             db.collection('api').doc('quotes').set(result.body.quoteResponse, {merge: true})
             .then(() => {
                 return res.status(200).json({success: "Success"});
+            }).catch(error => {
+                console.error(error);
+                return res.status(500).json({error: error.code}); 
+            })
+        });
+}
+
+
+/**
+ * Store the intraday values for a stock with an interval of 1 minute to the database
+ * Stored in:
+ *      Collection: "tickers"
+ *      Doc: name of the symbol
+ * 
+ * Request body:
+ * {
+ *     "ticker": "stock_symbol"
+ * }
+ */ 
+ exports.getTicker = (req, res) => {
+    let ticker = req.body.ticker;
+    //console.log(ticker);
+    if(ticker === undefined) throw new Error("No ticker entered.");
+
+    unirest
+        .get(`https://yahoo-finance-low-latency.p.rapidapi.com/v8/finance/chart/${ticker}`)
+        .query({
+            "interval": "1m",
+            "range": "1d",
+            "region": region,
+            "lang": lang
+        }).headers({
+            "x-rapidapi-key": API_KEY,
+            "x-rapidapi-host": "yahoo-finance-low-latency.p.rapidapi.com",
+            "useQueryString": true
+        }).end((result) => {
+            if (result.error) throw new Error(result.error);
+
+            tickerObj = 
+            {
+                dataGranularity: result.body.chart.result[0].meta.dataGranularity,
+                range: result.body.chart.result[0].meta.range,
+                timestamp: result.body.chart.result[0].timestamp,
+                indicators: 
+                {
+                    open: result.body.chart.result[0].indicators.quote[0].open,
+                    close: result.body.chart.result[0].indicators.quote[0].close,
+                    high: result.body.chart.result[0].indicators.quote[0].high,
+                    low: result.body.chart.result[0].indicators.quote[0].low,
+                    volume: result.body.chart.result[0].indicators.quote[0].volume
+                }
+            }
+            // console.log(result.body.chart.result[0].meta.tradingPeriods);
+            // console.log(result.body.chart.result[0].indicators.quote);
+            db.collection('tickers').doc(result.body.chart.result[0].meta.symbol).set(tickerObj)
+            .then(() => {
+                return res.status(201).json({success: `Intraday values of ${ticker} with an interval of 1 minute are saved to the database`});
             }).catch(error => {
                 console.error(error);
                 return res.status(500).json({error: error.code}); 
