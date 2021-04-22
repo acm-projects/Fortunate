9d60894900513d71115ef84231d8cf44250dc3fa
Nikita Istomin <nyi180000@utdallas.edu>
2021-04-22 13:14:27 -0500

    Minor changes with res object and async

--- backend/fortunate-functions/functions/index.js
+++ backend/fortunate-functions/functions/index.js
@@ -30,7 +30,7 @@ app.get("/valueot",FBAuth, getValueHistory);
 // Scheduled task to update tickers and user values
 exports.scheduledFunction = functions.pubsub.schedule('0 16 * * 1-5')
     .timeZone('America/New_York')
-    .onRun(updateTickersAndUserValues);
+    .onRun(await updateTickersAndUserValues);
 
 //app.get('/init', init);
 
