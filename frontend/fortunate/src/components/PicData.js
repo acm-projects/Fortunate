25fe2d9eee6d63bce027c0d9fa46ecf86d1110c2
TheRyanMills <rtmills09@gmail.com>
2021-04-28 17:45:38 -0500

    Text Changes

--- frontend/fortunate/src/components/PicData.js
+++ frontend/fortunate/src/components/PicData.js
@@ -7,46 +7,49 @@ import ImageFour from "../images/portfolio.JPG";
 import ImageFive from "../images/coinsM.jpg";
 
 export const PicData = [
-  {
-    title: "WELCOME TO FORTUNATE",
-    sub: "Learn the highs and lows of market trading in this interactive web game utilizing virtual currency.",
-    path: './home',
-    label: 'View Home',
-    image: ImageOne,
-    alt: "WELCOME"
-  },
-  {
-    title: "INTRO LESSONS",
-    sub: "Learn what is stock market.",
-    path: './learn/intro',
-    label: 'Intro Lessons',
-    image: ImageTwo,
-    alt: "Quote"
-  },
-  {
-    title: "TRADING LESSONS",
-    sub: "Learn the basics of the stock market with handcrafted lessons.",
-    path: './learn/trading',
-    label: 'Trading Lessons',
-    image: ImageFive,
-    alt: "Quote"
-  },
-  {
-    title: "VIRTUAL MARKET",
-    sub: "Apply your new skills using real market data from Fortune 500 companies.",
-    path: './vmpage',
-    label: 'View Market',
-    image: ImageThree,
-    alt: "Quote"
-  },
-  {
-    title: "PORTFOLIO",
-    sub: "Grow your portfolio over time as you build up your personal collection of equities.",
-    path: './portfolio',
-    label: 'View Portfolio',
-    image: ImageFour,
-    alt: "Quote"
-  }
+	{
+		title: "WELCOME TO FORTUNATE",
+		sub:
+			"Learn the highs and lows of market trading in this interactive web game.",
+		path: "./home",
+		label: "View Home",
+		image: ImageOne,
+		alt: "WELCOME",
+	},
+	{
+		title: "INTRO LESSONS",
+		sub: "Learn the ins and outs of the stock market.",
+		path: "./learn/intro",
+		label: "Intro Lessons",
+		image: ImageTwo,
+		alt: "Quote",
+	},
+	{
+		title: "TRADING LESSONS",
+		sub: "Learn the basics of the trading in the market.",
+		path: "./learn/trading",
+		label: "Trading Lessons",
+		image: ImageFive,
+		alt: "Quote",
+	},
+	{
+		title: "VIRTUAL MARKET",
+		sub:
+			"Apply your skills using real market data from Fortune 500 companies.",
+		path: "./vmpage",
+		label: "View Market",
+		image: ImageThree,
+		alt: "Quote",
+	},
+	{
+		title: "PORTFOLIO",
+		sub:
+			"Grow your portfolio over time as you build up your personal collection of stocks.",
+		path: "./portfolio",
+		label: "View Portfolio",
+		image: ImageFour,
+		alt: "Quote",
+	},
 ];
 /*
 import ImageOne from "./images/plantmonM.jpg";
